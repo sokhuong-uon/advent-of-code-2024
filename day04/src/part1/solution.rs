c60340506843fce0b85563a11e207884710b8c1b
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-12-09 14:35:54 +0700

    wip: find XMAS vertically

--- day04/src/part1/solution.rs
+++ day04/src/part1/solution.rs
@@ -12,11 +12,17 @@ fn solution(input: &str) -> u32 {
 
         for column in 0..line.len() {
             if line[column] == 'X' {
-                if find_xmas_to_the_right(&matrix, row, column) {
+                if is_xmas_right(&matrix, row, column) {
                     count += 1;
                 }
 
-                if find_xmas_to_the_left(&matrix, row, column) {
+                if is_xmas_left(&matrix, row, column) {
+                    count += 1;
+                }
+                if is_xmas_up(&matrix, row, column) {
+                    count += 1;
+                }
+                if is_xmas_down(&matrix, row, column) {
                     count += 1;
                 }
             }
@@ -30,7 +36,7 @@ fn convert_input_to_matrix(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn find_xmas_to_the_right(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+fn is_xmas_right(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
     if column + 3 >= matrix[row].len() {
         return false;
     }
@@ -48,7 +54,7 @@ fn find_xmas_to_the_right(matrix: &Vec<Vec<char>>, row: usize, column: usize) ->
     found
 }
 
-fn find_xmas_to_the_left(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+fn is_xmas_left(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
     if column < 3 {
         return false;
     }
@@ -58,6 +64,42 @@ fn find_xmas_to_the_left(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> 
 
     for i in 1..4 {
         if matrix[row][column - i] != xmas[i] {
+            found = false;
+            break;
+        }
+    }
+
+    found
+}
+
+fn is_xmas_up(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+    if row < 3 {
+        return false;
+    }
+
+    let xmas = vec!['X', 'M', 'A', 'S'];
+    let mut found = true;
+
+    for i in 1..4 {
+        if matrix[row - i][column] != xmas[i] {
+            found = false;
+            break;
+        }
+    }
+
+    found
+}
+
+fn is_xmas_down(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+    if row + 3 >= matrix.len() {
+        return false;
+    }
+
+    let xmas = vec!['X', 'M', 'A', 'S'];
+    let mut found = true;
+
+    for i in 1..4 {
+        if matrix[row + i][column] != xmas[i] {
             found = false;
             break;
         }
@@ -78,11 +120,11 @@ mod tests {
             vec!['M', 'S', 'A', 'S', 'X'],
             vec!['X', 'A', 'X', 'M', 'A'],
         ];
-        assert_eq!(find_xmas_to_the_right(&matrix, 1, 1), true);
-        assert_eq!(find_xmas_to_the_right(&matrix, 2, 4), false);
+        assert_eq!(is_xmas_right(&matrix, 1, 1), true);
+        assert_eq!(is_xmas_right(&matrix, 2, 4), false);
 
-        assert_eq!(find_xmas_to_the_right(&matrix, 3, 0), false);
-        assert_eq!(find_xmas_to_the_right(&matrix, 3, 2), false);
+        assert_eq!(is_xmas_right(&matrix, 3, 0), false);
+        assert_eq!(is_xmas_right(&matrix, 3, 2), false);
     }
 
     #[test]
@@ -93,12 +135,50 @@ mod tests {
             vec!['M', 'S', 'A', 'M', 'X'],
             vec!['X', 'A', 'M', 'X', 'A'],
         ];
-        assert_eq!(find_xmas_to_the_left(&matrix, 0, 1), false);
-        assert_eq!(find_xmas_to_the_left(&matrix, 1, 1), false);
+        assert_eq!(is_xmas_left(&matrix, 0, 1), false);
+        assert_eq!(is_xmas_left(&matrix, 1, 1), false);
 
-        assert_eq!(find_xmas_to_the_left(&matrix, 2, 4), true);
+        assert_eq!(is_xmas_left(&matrix, 2, 4), true);
 
-        assert_eq!(find_xmas_to_the_left(&matrix, 3, 0), false);
-        assert_eq!(find_xmas_to_the_left(&matrix, 3, 3), false);
+        assert_eq!(is_xmas_left(&matrix, 3, 0), false);
+        assert_eq!(is_xmas_left(&matrix, 3, 3), false);
+    }
+
+    #[test]
+    fn test_find_xmas_up() {
+        let matrix = vec![
+            vec!['S', 'X', 'S', 'A', 'M'],
+            vec!['A', 'X', 'M', 'A', 'S'],
+            vec!['M', 'A', 'X', 'M', 'X'],
+            vec!['X', 'M', 'M', 'X', 'A'],
+            vec!['X', 'X', 'X', 'M', 'A'],
+        ];
+        assert_eq!(is_xmas_up(&matrix, 3, 0), true);
+        assert_eq!(is_xmas_up(&matrix, 0, 1), false);
+        assert_eq!(is_xmas_up(&matrix, 1, 1), false);
+
+        assert_eq!(is_xmas_up(&matrix, 4, 2), false);
+
+        assert_eq!(is_xmas_up(&matrix, 3, 3), false);
+    }
+
+    #[test]
+    fn test_find_xmas_down() {
+        let matrix = vec![
+            vec!['S', 'X', 'S', 'A', 'M'],
+            vec!['A', 'M', 'M', 'X', 'S'],
+            vec!['M', 'A', 'X', 'M', 'X'],
+            vec!['X', 'S', 'M', 'X', 'M'],
+            vec!['X', 'X', 'X', 'M', 'A'],
+        ];
+        assert_eq!(is_xmas_down(&matrix, 3, 0), false);
+        assert_eq!(is_xmas_down(&matrix, 4, 0), false);
+
+        assert_eq!(is_xmas_down(&matrix, 0, 1), true);
+
+        assert_eq!(is_xmas_down(&matrix, 2, 2), false);
+        assert_eq!(is_xmas_down(&matrix, 4, 2), false);
+
+        assert_eq!(is_xmas_down(&matrix, 2, 4), false);
     }
 }

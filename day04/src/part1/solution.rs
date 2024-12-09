42bf712054e41176e8b0b9d819ae024a1caad2c5
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-12-09 15:59:38 +0700

    feat: complete day 4 part 1

--- day04/src/part1/solution.rs
+++ day04/src/part1/solution.rs
@@ -7,29 +7,37 @@ pub fn main() -> String {
 fn solution(input: &str) -> u32 {
     let matrix = convert_input_to_matrix(input);
 
-    matrix.iter().enumerate().fold(0, |acc, (row, line)| {
-        let mut count = 0;
-
-        for column in 0..line.len() {
-            if line[column] == 'X' {
-                if is_xmas_right(&matrix, row, column) {
-                    count += 1;
-                }
-
-                if is_xmas_left(&matrix, row, column) {
-                    count += 1;
-                }
-                if is_xmas_up(&matrix, row, column) {
-                    count += 1;
-                }
-                if is_xmas_down(&matrix, row, column) {
-                    count += 1;
-                }
-            }
-        }
+    matrix.iter().enumerate().fold(0, |acc, (row, _)| {
+        let count = check_xmases(&matrix, row);
 
         acc + count
     })
+}
+
+fn check_xmases(matrix: &Vec<Vec<char>>, row: usize) -> u32 {
+    let checks = [
+        is_xmas_right,
+        is_xmas_left,
+        is_xmas_up,
+        is_xmas_down,
+        is_xmas_first_quadrant,
+        is_xmas_second_quadrant,
+        is_xmas_third_quadrant,
+        is_xmas_fourth_quadrant,
+    ];
+
+    let mut count = 0;
+
+    for column in 0..matrix[row].len() {
+        if matrix[row][column] == 'X' {
+            count += checks
+                .iter()
+                .filter(|&check| check(&matrix, row, column))
+                .count();
+        }
+    }
+
+    count as u32
 }
 
 fn convert_input_to_matrix(input: &str) -> Vec<Vec<char>> {
@@ -108,6 +116,85 @@ fn is_xmas_down(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
     found
 }
 
+fn is_xmas_first_quadrant(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+    if row < 3 {
+        return false;
+    }
+
+    if column + 3 >= matrix[row].len() {
+        return false;
+    }
+
+    let xmas = vec!['X', 'M', 'A', 'S'];
+    let mut found = true;
+
+    for i in 1..4 {
+        if matrix[row - i][column + i] != xmas[i] {
+            found = false;
+            break;
+        }
+    }
+
+    found
+}
+
+fn is_xmas_second_quadrant(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+    if column < 3 || row < 3 {
+        return false;
+    }
+
+    let xmas = vec!['X', 'M', 'A', 'S'];
+    let mut found = true;
+
+    for i in 1..4 {
+        if matrix[row - i][column - i] != xmas[i] {
+            found = false;
+            break;
+        }
+    }
+
+    found
+}
+
+fn is_xmas_third_quadrant(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+    if column < 3 {
+        return false;
+    }
+
+    if row + 3 >= matrix.len() {
+        return false;
+    }
+
+    let xmas = vec!['X', 'M', 'A', 'S'];
+    let mut found = true;
+
+    for i in 1..4 {
+        if matrix[row + i][column - i] != xmas[i] {
+            found = false;
+            break;
+        }
+    }
+
+    found
+}
+
+fn is_xmas_fourth_quadrant(matrix: &Vec<Vec<char>>, row: usize, column: usize) -> bool {
+    if row + 3 >= matrix.len() || column + 3 >= matrix[row].len() {
+        return false;
+    }
+
+    let xmas = vec!['X', 'M', 'A', 'S'];
+    let mut found = true;
+
+    for i in 1..4 {
+        if matrix[row + i][column + i] != xmas[i] {
+            found = false;
+            break;
+        }
+    }
+
+    found
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -180,5 +267,79 @@ mod tests {
         assert_eq!(is_xmas_down(&matrix, 4, 2), false);
 
         assert_eq!(is_xmas_down(&matrix, 2, 4), false);
+    }
+
+    #[test]
+    fn test_is_xmas_first_quadrant() {
+        let matrix = vec![
+            vec!['S', 'X', 'S', 'S', 'M'],
+            vec!['X', 'M', 'A', 'X', 'X'],
+            vec!['M', 'M', 'X', 'A', 'A'],
+            vec!['X', 'S', 'M', 'M', 'M'],
+            vec!['X', 'X', 'X', 'M', 'X'],
+        ];
+        assert_eq!(is_xmas_first_quadrant(&matrix, 1, 0), false);
+        assert_eq!(is_xmas_first_quadrant(&matrix, 0, 1), false);
+
+        assert_eq!(is_xmas_first_quadrant(&matrix, 4, 2), false);
+        assert_eq!(is_xmas_first_quadrant(&matrix, 4, 4), false);
+
+        assert_eq!(is_xmas_first_quadrant(&matrix, 3, 0), true);
+        assert_eq!(is_xmas_first_quadrant(&matrix, 4, 1), false);
+    }
+
+    #[test]
+    fn test_is_xmas_second_quadrant() {
+        let matrix = vec![
+            vec!['S', 'X', 'S', 'S', 'M'],
+            vec!['X', 'S', 'A', 'X', 'X'],
+            vec!['A', 'M', 'A', 'A', 'A'],
+            vec!['X', 'M', 'M', 'M', 'M'],
+            vec!['X', 'X', 'X', 'M', 'X'],
+        ];
+        assert_eq!(is_xmas_second_quadrant(&matrix, 1, 0), false);
+        assert_eq!(is_xmas_second_quadrant(&matrix, 0, 1), false);
+        assert_eq!(is_xmas_second_quadrant(&matrix, 0, 4), false);
+
+        assert_eq!(is_xmas_second_quadrant(&matrix, 4, 2), false);
+        assert_eq!(is_xmas_second_quadrant(&matrix, 4, 4), true);
+    }
+
+    #[test]
+    fn test_is_xmas_third_quadrant() {
+        let matrix = vec![
+            vec!['S', 'X', 'S', 'S', 'X'],
+            vec!['X', 'S', 'X', 'M', 'X'],
+            vec!['M', 'M', 'A', 'M', 'S'],
+            vec!['A', 'S', 'A', 'S', 'A'],
+            vec!['X', 'A', 'M', 'X', 'S'],
+        ];
+        assert_eq!(is_xmas_third_quadrant(&matrix, 0, 1), false);
+        assert_eq!(is_xmas_third_quadrant(&matrix, 1, 0), false);
+        assert_eq!(is_xmas_third_quadrant(&matrix, 4, 0), false);
+        assert_eq!(is_xmas_third_quadrant(&matrix, 4, 3), false);
+
+        assert_eq!(is_xmas_third_quadrant(&matrix, 1, 2), false);
+
+        assert_eq!(is_xmas_third_quadrant(&matrix, 0, 4), true);
+        assert_eq!(is_xmas_third_quadrant(&matrix, 1, 4), false);
+    }
+
+    #[test]
+    fn test_is_xmas_fourth_quadrant() {
+        let matrix = vec![
+            vec!['S', 'X', 'M', 'S', 'M'],
+            vec!['X', 'S', 'A', 'M', 'X'],
+            vec!['X', 'M', 'X', 'A', 'S'],
+            vec!['M', 'M', 'A', 'M', 'X'],
+            vec!['X', 'A', 'A', 'S', 'S'],
+        ];
+        assert_eq!(is_xmas_fourth_quadrant(&matrix, 4, 0), false);
+        assert_eq!(is_xmas_fourth_quadrant(&matrix, 2, 0), false);
+
+        assert_eq!(is_xmas_fourth_quadrant(&matrix, 1, 0), true);
+
+        assert_eq!(is_xmas_fourth_quadrant(&matrix, 0, 1), false);
+        assert_eq!(is_xmas_fourth_quadrant(&matrix, 1, 4), false);
     }
 }
